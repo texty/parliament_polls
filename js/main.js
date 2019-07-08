e93b6f07188ccf6e4334e9885e7d149bcbaec634
Vladislav <drimacus182@gmail.com>
2019-07-08 19:28:10 +0300

    дані, класи, заміна точок у назвах філдів

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 (function() {
 
-    var candidates_1_checked = ["Tymoshenko", "Poroshenko", "Zelensky", "Undecided"];
-    var candidates_1_unchecked = ["Will_not_vote", "Grytsenko", "Boyko", "Lyashko"];
+    var candidates_1_checked = ["Слуга__народу", "Опозиційна__платформа", "Європейська__солідарність"];
+    var candidates_1_unchecked = ["Батьківщина"];
 
     var candidates_2_checked = ["Tymoshenko", "Poroshenko", "Zelensky", "Undecided"];
     var candidates_2_unchecked = ["Grytsenko", "Boyko", "Lyashko"];
@@ -9,42 +9,51 @@
     var candidates_3_checked = ["Tymoshenko", "Poroshenko", "Zelensky"];
     var candidates_3_unchecked = ["Grytsenko", "Boyko", "Lyashko"];
 
-    var all_candidates = [ "Will_not_vote", "Tymoshenko", "Poroshenko", "Zelensky", "Undecided",
-        "Grytsenko", "Boyko", "Lyashko"];
+    var all_candidates = [ "Слуга__народу", "Опозиційна__платформа", "Європейська__солідарність",
+        "Батьківщина", "Сила__і__Честь", "Громадянська__позиція", "Опозиційний__блок",
+        "Радикальна__партія","Українська__стратегія","Свобода","Самопоміч",
+        "Голос____Вакарчук____","Не__визначились","Не__піду__на__вибори"];
 
     var display_names = __page_locale__.display_names;
     var data_root = __page_locale__.data_root;
 
     var charts = {};
     
-    fetchData("2", function(err, raw_data_lines, raw_data_points){
-        charts["2"] = makeChart(raw_data_lines, raw_data_points, candidates_2_checked,
-            candidates_2_unchecked,[0,27], [0,3,6,9,12,15,18,21,24,27]);
+    // fetchData("2", function(err, raw_data_lines, raw_data_points){
+    //     charts["2"] = makeChart(raw_data_lines, raw_data_points, candidates_2_checked,
+    //         candidates_2_unchecked,[0,27], [0,3,6,9,12,15,18,21,24,27]);
+    //
+    //     var chart_select = d3.select("#chart-select").on("change", function(){
+    //         drawChart(charts[chart_select.node().value])
+    //     });
+    //
+    //     drawChart(charts[chart_select.node().value]);
+    //
+    //     fetchData("1", function(err, raw_data_lines, raw_data_points) {
+    //         charts["1"] = makeChart(raw_data_lines, raw_data_points, candidates_1_checked,
+    //             candidates_1_unchecked, [0, 27], [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]);
+    //     });
+    //
+    //     fetchData("3", function(err, raw_data_lines, raw_data_points) {
+    //         charts["3"] = makeChart(raw_data_lines, raw_data_points, candidates_3_checked,
+    //             candidates_3_unchecked, [0, 30], [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]);
+    //     });
+    // });
 
-        var chart_select = d3.select("#chart-select").on("change", function(){
-            drawChart(charts[chart_select.node().value])
-        });
+    fetchData("1", function(err, raw_data_lines, raw_data_points) {
+        charts["1"] = makeChart(raw_data_lines, raw_data_points, candidates_1_checked,
+            candidates_1_unchecked, [0, 45], [0, 10, 20, 30, 40]);
 
-        drawChart(charts[chart_select.node().value]);
-
-        fetchData("1", function(err, raw_data_lines, raw_data_points) {
-            charts["1"] = makeChart(raw_data_lines, raw_data_points, candidates_1_checked,
-                candidates_1_unchecked, [0, 27], [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]);
-        });
-
-        fetchData("3", function(err, raw_data_lines, raw_data_points) {
-            charts["3"] = makeChart(raw_data_lines, raw_data_points, candidates_3_checked,
-                candidates_3_unchecked, [0, 30], [0, 3, 6, 9, 12, 15, 18, 21, 24, 27, 30]);
-        });
+        drawChart(charts["1"]);
     });
 
     function drawChart(chart) {
         d3.select("#main_chart_vertical").selectAll("svg").remove();
         d3.select("#main_chart_vertical").call(chart);
-
-        d3.selectAll("svg .axis--y--labels .tick text").filter(function(){
-            return ['березень', 'March'].indexOf(d3.select(this).text()) >= 0;
-        }).attr("dy", "0.7em");
+        //
+        // d3.selectAll("svg .axis--y--labels .tick text").filter(function(){
+        //     return ['березень', 'March'].indexOf(d3.select(this).text()) >= 0;
+        // }).attr("dy", "0.7em");
     }
 
     function makeChart(raw_data_lines, raw_data_points, candidates_checked,
@@ -132,13 +141,29 @@
                     d.lower = + d.lower;
                     d.upper = + d.upper;
                     d.date = new Date(d.date);
+
+                    // Перейменуємо усі стовпчики із точкою у назві
+                    d.candidate = d.candidate.replace(/\./g, "__");
                     delete d[""];
+                });
+
+
+                // Перейменуємо усі стовпчики із точкою у назві
+                raw_data_points.forEach(function (d) {
+                    delete d[""];
+
+                    Object.keys(d).forEach(function (k) {
+                        var buf = d[k];
+                        delete d[k];
+                        d[k.replace(/\./g, "__")] = buf;
+                    });
                 });
 
                 raw_data_points.forEach(function(d) {
                     d.date = new Date(d.end_date);
-                    all_candidates.forEach(function(candidate) {d[candidate] = toNumber(d[candidate])});
-                    delete d[""];
+                    all_candidates.forEach(function(candidate) {
+                        d[candidate] = toNumber(d[candidate])
+                    });
                 });
                 
                 return cb(err, raw_data_lines, raw_data_points);
